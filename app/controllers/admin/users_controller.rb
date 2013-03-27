e3796d519738b425a831c976ca2f420bfc7d3900
Jael <cutejael@gmail.com>
2013-03-27 21:45:21 +0800

    Added ability to edit and update users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < Admin::BaseController
+  before_filter :find_user, only: [:show, :edit, :update, :destroy]
   def index
     @users = User.order(:email)
   end
@@ -16,5 +17,28 @@ class Admin::UsersController < Admin::BaseController
       flash[:alert] = "User has not been created."
       render :new
     end
+  end
+
+  def update
+    @user.admin = params[:user][:admin] == '1'
+    params[:user].delete(:admin)
+    if params[:user][:password].blank?
+       params[:user].delete(:password)
+       params[:user].delete(:password_confirmation)
+    end 
+    if @user.update_attributes(params[:user])
+      redirect_to admin_users_path,  notice: "User has been updated."
+    else
+      flash[:alert] = "User has not been updated."
+      render :edit
+    end
+    p User.all
+  end
+
+
+  
+  private
+  def find_user
+    @user = User.find(params[:id])
   end
 end
--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -2,11 +2,15 @@ Given(/^there are the following users:$/) do |table|
   table.hashes.each do |attributes|
     unconfirmed = attributes.delete('unconfirmed') == 'true'
     p attributes
-    @user = User.create!(email: attributes[:email], password: attributes[:password])
+    @user = User.create!(email: attributes[:email], password: attributes[:password], password_confirmation: attributes[:password_confirmation])
     @user.update_attribute(:admin, attributes[:admin] == "true")
     @user.confirm! unless unconfirmed
   end
 end
 When(/^I check "(.*?)"$/) do |arg1|
   check arg1
+end
+When(/^I press the "(.*?)" with user_id "(.*?)"$/) do |arg1, arg2|
+  click_button arg1
+  User.find(arg2).confirm!
 end
