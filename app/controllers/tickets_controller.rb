a5de1ce08b06123f64d698c9bf456be1b708679a
Jael <cutejael@gmail.com>
2013-03-29 00:52:20 +0800

    fix the bug

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,10 +3,7 @@ class TicketsController < ApplicationController
   before_filter :find_project
   before_filter :find_ticket, only: [:show, :edit, :update, :destroy]
   before_filter :authorize_create!, only: [:new, :create]
-<<<<<<< HEAD
   before_filter :authorize_update!, only: [:edit, :update]
-=======
->>>>>>> 6ba6e4d2e70eec81a92e2ca8e8a6de3a70f935e3
   def new
     @ticket = Ticket.new
   end 
@@ -53,7 +50,6 @@ class TicketsController < ApplicationController
       redirect_to @project
     end
   end
-<<<<<<< HEAD
 
   def authorize_update!
     if !current_user.admin? && cannot?("edit tickets".to_sym, @project)
@@ -61,6 +57,4 @@ class TicketsController < ApplicationController
       redirect_to @project
     end
   end
-=======
->>>>>>> 6ba6e4d2e70eec81a92e2ca8e8a6de3a70f935e3
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
   has_many :tickets, dependent: :destroy
-<<<<<<< HEAD
-  has_many :permissions
-=======
   has_many :permissions  
->>>>>>> 6ba6e4d2e70eec81a92e2ca8e8a6de3a70f935e3
   def to_s
     "#{email}(#{admin? ? "Admin" : "User"})"
   end
