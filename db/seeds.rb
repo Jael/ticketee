2774ebf27c466ea8a3f92407acab53a489ab2a7d
Jael <cutejael@gmail.com>
2013-04-07 10:34:52 +0800

    add the ajax,but something wrong

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
-admin_user = User.create(email: "admin@ticketee.com", password: "password")
-admin_user.admin = true
-admin_user.confirm!
-Project.create(name: "Ticketee Beta")
+#admin_user = User.create(email: "admin@ticketee.com", password: "password")
+#admin_user.admin = true
+#admin_user.confirm!
+#Project.create(name: "Ticketee Beta")
+
+new_user = User.create(email: "new_user@ticketee.com", password: "password")
+new_user.confirm!
